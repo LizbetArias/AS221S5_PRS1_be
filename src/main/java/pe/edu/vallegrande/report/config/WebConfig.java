599e640516845cb5ec4feaa65059e67fa8c7a990
Lizbet Arias Tinco <127221079+LizbetArias@users.noreply.github.com>
2024-12-04 04:46:50 +0000

    Docker-compose/kubernetes-LISTO

--- src/main/java/pe/edu/vallegrande/report/config/WebConfig.java
+++ src/main/java/pe/edu/vallegrande/report/config/WebConfig.java
@@ -1,15 +1,17 @@
 package pe.edu.vallegrande.report.config;
+
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.config.CorsRegistry;
 import org.springframework.web.reactive.config.WebFluxConfigurer;
 
 @Configuration
 public class WebConfig implements WebFluxConfigurer {
+
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins("http://localhost:4200")
-                .allowedMethods("GET", "POST", "PUT", "DELETE")
-                .allowedHeaders("*");
+        registry.addMapping("/**") // Permite todas las rutas
+                .allowedOrigins("*") // Permite solicitudes desde cualquier origen
+                .allowedMethods("*") // Permite todos los métodos HTTP (GET, POST, PUT, DELETE, etc.)
+                .allowedHeaders("*"); // Permite todos los encabezados
     }
 }
